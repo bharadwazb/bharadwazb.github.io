465de24ea25273cdebe226277c31f0f0073406cd
bharadwazb <bhumabharadwaz@gmail.com>
2026-02-19 23:19:23 +0530

    Vite-app

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 export default function NavBar() {
     return (
         <nav className="navbar">
-            <span className="navbar__brand">bharadwazb.dev</span>
+            <span className="navbar__brand">bharadwazb</span>
             <ul className="navbar__links">
                 <li><NavLink to="/" end>Home</NavLink></li>
                 <li><NavLink to="/experience"    >Experience</NavLink></li>
